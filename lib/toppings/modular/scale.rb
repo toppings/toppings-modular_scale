daa353f102c4b3263b62f3d449c5dc040ea18c9f
Falk Hoppe <falkhoppe81@gmail.com>
2013-08-28 13:37:51 +0200

    initial commit

---
+++ lib/toppings/modular/scale.rb
@@ -0,0 +1,9 @@
+require "toppings/modular/scale/version"
+
+module Toppings
+  module Modular
+    module Scale
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/toppings/modular/scale/version.rb
@@ -0,0 +1,7 @@
+module Toppings
+  module Modular
+    module Scale
+      VERSION = "0.0.1"
+    end
+  end
+end
